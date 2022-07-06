d354269f6a1121d397b836197b836b9c34fab199
OleksandrRomaniuk230 <itspace.001@gmail.com>
2022-07-06 14:33:27 +0300

    delete files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+
 import misclick from './plugins/misclick'
 
 Vue.config.productionTip = false
@@ -9,7 +8,5 @@ Vue.config.productionTip = false
 Vue.use(misclick)
 
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount('#app')
